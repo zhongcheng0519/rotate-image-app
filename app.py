ff98ddc8ac2bf8d413c21493e44aedbde4943ab3
钟宬 <zhongcheng0519@163.com>
2023-09-22 20:59:28 +0800

    rename Hello

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 import cv2
 from PIL import Image
@@ -19,7 +5,7 @@ import numpy as np
 
 
 def rot_degree(img, degree):
-    rows, cols = img.shape
+    rows, cols = img.shape[0:2]
     center = (cols / 2, rows / 2)
     mask = img.copy()
     mask[:, :] = 255
@@ -42,15 +28,12 @@ def rot_degree(img, degree):
 
 def run():
     st.title("Image Rotate Application")
+    st.write("rotate anti-clockwise.")
     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-    degree_value = st.slider('degree', min_value=0, max_value=360, value=50)
+    degree_value = st.slider('degree', min_value=-180, max_value=180, value=50)
 
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file).convert('L')
-        st.image(image, caption='Uploaded Image.', use_column_width=True)
-        st.write("")
-        st.write("Processing...")
-
+    if st.button('Rotate', use_container_width=True) and uploaded_file is not None:
+        image = Image.open(uploaded_file)
         result_img, _ = rot_degree(np.array(image), degree_value)
 
         col1, col2 = st.columns(2)
